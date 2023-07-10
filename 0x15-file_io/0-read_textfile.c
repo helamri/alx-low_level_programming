0fb96f6ef6faea9ff4e452a4a1af4ad40d06c7e7
helamri <elamri.hamza@outlook.fr>
2023-07-10 14:41:55 +0100

    first

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,52 +1,28 @@
-#include <stdio.h>
+#include "main.h"
 #include <stdlib.h>
-#include <fcntl.h>
-#include <unistd.h>
 
 /**
- * read_textfile - function that reads a text file and prints it
- * to the POSIX standard output
- * @filename: the text file
- * @letters: the number of letters
- *
- * Return: the actual number of letters it could read and print,
- * if the file can not be opened or read, or the filename is NULL,
- * or write fails or does not write the expected amount of bytes return 0
+ * read_textfile- Read text file print to STDOUT.
+ * @filename: text file being read
+ * @letters: number of letters to be read
+ * Return: w- actual number of bytes read and printed
+ *        0 when function fails or filename is NULL.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-int fd;
-char *buf;
-ssize_t bytes_read, bytes_written;
-if (filename == NULL)
-{
-return (0);
-}
-fd = open(filename, O_RDONLY);
-if (fd == -1)
-{
-return (0);
-}
-buf = malloc(sizeof(char) * letters);
-if (buf == NULL)
-{
-return (0);
-}
-bytes_read = read(fd, buf, letters);
-if (bytes_read == -1)
-{
-free(buf);
-close(fd);
-return (0);
-}
-bytes_written = write(STDOUT_FILENO, buf, bytes_read);
-if (bytes_written == -1 || bytes_written != bytes_read)
-{
-free(buf);
-close(fd);
-return (0);
-}
-free(buf);
-close(fd);
-return (bytes_written);
+	char *buf;
+	ssize_t fd;
+	ssize_t w;
+	ssize_t t;
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	buf = malloc(sizeof(char) * letters);
+	t = read(fd, buf, letters);
+	w = write(STDOUT_FILENO, buf, t);
+
+	free(buf);
+	close(fd);
+	return (w);
 }
--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,44 +1,33 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <stddef.h>
 
 /**
- * create_file - function that creates a file with specific permissions
- * and writes text to it
- * @filename: name of the file to create
- * @text_content: content to write to the file
+ * create_file - Creates a file.
+ * @filename: A pointer to the name of the file to create.
+ * @text_content: A pointer to a string to write to the file.
  *
- * Return: 1 on success, -1 on failure
+ * Return: If the function fails - -1.
+ *         Otherwise - 1.
  */
 int create_file(const char *filename, char *text_content)
 {
-int fd, len, wrlen;
-if (!filename)
-{
-return (-1);
-}
-fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-len = 0;
-if (fd == -1)
-{
-return (-1);
-}
-if (text_content)
-{
-while (text_content[len])
-{
-len++;
-}
-wrlen = write(fd, text_content, len);
-if (wrlen == -1)
-{
-close(fd);
-return (-1);
-}
-}
-close(fd);
-return (1);
+	int fd, w, len = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
+
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(fd, text_content, len);
+
+	if (fd == -1 || w == -1)
+		return (-1);
+
+	close(fd);
+
+	return (1);
 }
